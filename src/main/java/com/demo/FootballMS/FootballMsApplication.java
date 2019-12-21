b5b16f966e469ca1e3abaf594f7c48f2e53d031d
PSEH-5 <pseh-5@123>
2019-12-21 11:15:55 +0530

    init repo

---
+++ src/main/java/com/demo/FootballMS/FootballMsApplication.java
@@ -0,0 +1,13 @@
+package com.demo.FootballMS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FootballMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FootballMsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demo/FootballMS/FootballMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.FootballMS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FootballMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
